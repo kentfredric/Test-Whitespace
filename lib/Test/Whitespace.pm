058fc07e267e6252bf37e3538d91a9c091b60562
Kent Fredric <kentfredric@gmail.com>
2009-07-16 02:49:15 +1200

    Build of a339305e8f3debc5cdb4cb1b47525eb3f1c5c221

---
+++ lib/Test/Whitespace.pm
@@ -0,0 +1,37 @@
+package Test::Whitespace;
+our $VERSION = '0.0100';
+
+
+# ABSTRACT: Check your dist for excess whitespace.
+
+1;
+
+
+
+
+=pod
+
+=head1 NAME
+
+Test::Whitespace - Check your dist for excess whitespace.
+
+=head1 VERSION
+
+version 0.0100
+
+=head1 AUTHOR
+
+  Kent Fredric <kentnl at cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+
+__END__
